3ec59297e9160189d232c1ad5c99a5cb55b3e0e1
Mimozai <mimoza.berisha11@student.uni-pr.edu>
2026-01-03 13:52:34 +0100

    Kompletimi i logjikës së lojës Tic-Tac-Toe - Dorëzim Individual
    Kam shtuar funksionet checkWin, checkDraw dhe ciklin kryesor main për ta bërë projektin plotësisht funksional.

--- Source1.cpp
+++ Source1.cpp
@@ -9,7 +9,7 @@ char board[3][3] = {
 
 char currentPlayer = 'X';
 
-
+// Display the board
 void displayBoard() {
     cout << "\n";
     for (int i = 0; i < 3; i++) {
@@ -24,12 +24,12 @@ void displayBoard() {
     cout << "\n";
 }
 
-
+// Change player
 void switchPlayer() {
     currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
 }
 
-
+// Make a move
 bool makeMove(int choice) {
     int row = (choice - 1) / 3;
     int col = (choice - 1) % 3;
@@ -39,4 +39,73 @@ bool makeMove(int choice) {
         return true;
     }
     return false;
+}
+
+// Check win
+bool checkWin() {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == currentPlayer &&
+            board[i][1] == currentPlayer &&
+            board[i][2] == currentPlayer)
+            return true;
+
+        if (board[0][i] == currentPlayer &&
+            board[1][i] == currentPlayer &&
+            board[2][i] == currentPlayer)
+            return true;
+    }
+
+    if (board[0][0] == currentPlayer &&
+        board[1][1] == currentPlayer &&
+        board[2][2] == currentPlayer)
+        return true;
+
+    if (board[0][2] == currentPlayer &&
+        board[1][1] == currentPlayer &&
+        board[2][0] == currentPlayer)
+        return true;
+
+    return false;
+}
+
+// Check draw
+bool checkDraw() {
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] != 'X' && board[i][j] != 'O')
+                return false;
+    return true;
+}
+
+int main() {
+    int choice;
+
+    cout << "=== TIC TAC TOE (X - O) ===\n";
+
+    while (true) {
+        displayBoard();
+        cout << "Player " << currentPlayer << ", choose a position (1-9): ";
+        cin >> choice;
+
+        if (choice < 1 || choice > 9 || !makeMove(choice)) {
+            cout << "Invalid move! Try again.\n";
+            continue;
+        }
+
+        if (checkWin()) {
+            displayBoard();
+            cout << "Player " << currentPlayer << " wins!\n";
+            break;
+        }
+
+        if (checkDraw()) {
+            displayBoard();
+            cout << "It's a draw!\n";
+            break;
+        }
+
+        switchPlayer();
+    }
+
+    return 0;
 }
